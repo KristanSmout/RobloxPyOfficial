ebdb0b93c22b7af4671ced2161ae0f3c1a8e3379
anytarsier67 <42589471+anytarseir67@users.noreply.github.com>
2021-11-04 15:42:06 -0400

    bump version, fix typo in README, update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
+import pathlib
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 with open('requirements.txt') as f:
   requirements = f.read().splitlines()
 
 setup(name='robloxpy',
-    version='1.0.0.a1',
+    version='1.0.0',
     description='RobloxPy is an object oriented python wrapper for the roblox API.',
+    long_description=README,
+    long_description_content_type="text/markdown",
     author='KristanSmout & anytarseir67',
     author_email = '',
     url='https://github.com/KristanSmout/RobloxPyOfficial/tree/1.0',
